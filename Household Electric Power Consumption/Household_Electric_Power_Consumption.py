3c77bd2088a5fcaa8e054c699f242383b08349a1
rttrif <rttrif@rambler.ru>
2021-07-28 16:22:50 +0300

    load data

--- Household Electric Power Consumption/Household_Electric_Power_Consumption.py
+++ Household Electric Power Consumption/Household_Electric_Power_Consumption.py
@@ -1,6 +1,27 @@
+'''
+HOUSEHOLD ELECTRIC POWER CONSUMPTION
+
+Solving the regression problem using fully connected artificial neural networks.
+'''
+
 import tensorflow as tf
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-print(tf.__version__)
+# %%
+# READ DATA
+data_path = "Household Electric Power Consumption/household_power_consumption.txt"
+
+data_frame = pd.read_csv(data_path, sep=';')
+
+head = data_frame.head().T
+
+tail = data_frame.tail().T
+# %%
+# EDA
+
+
 
 
